739ad0b3aa0bf2a43381ad267e9180ae15694dfa
Ihor Voznyi <ihor.voznyi.dev@gmail.com>
2024-11-18 23:35:48 +0200

    initial setup

---
+++ apps/endous-finet-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'endous-finet-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/endous-finet-api',
+};
